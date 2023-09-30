edeed617b7ee3d534a98488c80e57fd6735d28e8
unknown <waqasahmedwikki15@gmail.com>
2023-09-30 20:56:56 +0500

    legal and illigal variables

---
+++ app.js
@@ -0,0 +1,33 @@
+            //Chapter 4 quastin 1//
+var town = "Nazimabad ";
+var city = "karachi ";
+var country = "Pakistan";
+alert(town +city+country);
+
+            //Chapter 4 quastin 2//
+
+            //legal variable name//
+let book = "Quaran pak";
+var country8 = "pakistan"; 
+const my1Name = "Waqas Ahmed";
+let owais_Raza_Qadri = "World Best Naat Khuwan";
+const myLove = "My Country";
+
+            //illegal variable name//
+//let 5th = "panchwen manzil";
+//var kibab-case = "not allowed";
+//var delete = "sylani";
+//const FULLNAME = "waqasahmed";
+//console.log(fullName);
+            //Chapter 4 quastin 3//
+
+                 //q 2 part A//
+document.write("<h1>“Rules for naming JS variables”</h1>");
+                //q 2 part B//
+document.write("<h3>Variable names can only contain letters,numbers,dollar sings and underscores.For example $my_1stVariable</h3>");
+                //q 2 part C//
+document.write("<h3>Variables must begin with a latter,underscores or dollar sing. For example $name, _name or name")
+                //q 2 part D//
+document.write("<h3>Variable names are case sansitive</h3>")
+                //q 2 part E//
+document.write("<h3>Variable names should not be JS keyword</h3>")
